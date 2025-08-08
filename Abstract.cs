8f366f716754059c01e2f2084c6dbc4fc7d61c35
Swaroop K R <141357728+swar41@users.noreply.github.com>
2025-08-08 11:17:44 +0530

    Add files via upload

---
+++ Abstract.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp1
+{
+    abstract class AbstractClass
+    {
+        // Abstract classes cannot be instantiated directly.
+        public void ConcreteMethod()
+        {
+            Console.WriteLine("This is a concrete method in an abstract class.");
+        }
+        public abstract void AbstractMethod();
+
+
+    }
+    class DerivedClass : AbstractClass
+    {
+        public override void AbstractMethod()
+        {
+            Console.WriteLine("This is an implementation of the abstract method.");
+        }
+    }
+    internal class Abstract
+    {
+        static void Main(string[] args)
+        {
+            // AbstractClass ac = new AbstractClass(); // This line will cause a compile-time error because you cannot instantiate an abstract class.
+            DerivedClass dc = new DerivedClass();
+            dc.ConcreteMethod(); // Calling the concrete method from the abstract class
+            dc.AbstractMethod(); // Calling the overridden abstract method
+        }
+    }
+}
